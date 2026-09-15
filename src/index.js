02a54f4a83ad965ec1d5573986f8ebb94cfa3507
christopherstrasser-sudo <christopher.strasser@gmx.de>
2026-09-15 14:13:50 +0200

    feat: activate Creator Hub v0.9.2 runtime

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ const { attachRoleStudioApi } = require('./role-studio-api');
 const { attachEmojiApi } = require('./emoji-api');
 const { attachTicketStudioApi } = require('./ticket-studio-api');
 const { attachTicketRuntime } = require('./ticket-studio-runtime');
-const { attachCreatorHubApi } = require('./creator-hub-api');
-const { startCreatorRuntime } = require('./creator-runtime');
+const { attachCreatorHubApi } = require('./creator-hub-api-v092');
+const { startCreatorRuntime } = require('./creator-runtime-v092');
 
 function isDisallowedIntentError(error) {
   const message = String(error?.message || '');
