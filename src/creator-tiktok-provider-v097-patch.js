8a9a84bc2e83e58701efbec8fbebd1ac10a5128e
christopherstrasser-sudo <christopher.strasser@gmx.de>
2026-09-15 18:42:06 +0200

    fix: install TikTok browser-session API fallback before provider load

--- src/creator-tiktok-provider-v097-patch.js
+++ src/creator-tiktok-provider-v097-patch.js
@@ -1,4 +1,5 @@
 const legacy = require('./creator-tiktok-provider');
+require('./tiktok-signature-browser-fetch').installBrowserFetchFallback();
 const next = require('./creator-tiktok-provider-v097');
 Object.assign(legacy, next);
 module.exports = legacy;
